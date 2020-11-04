526aef551e5afe97d39c3a02ee9865655a3f64f3
Dave Beyer <dave.beyer@gmail.com>
2020-11-03 18:50:09 -0800

    set default toolbar, font familly options, font sizes, and image styling

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -7,88 +7,157 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+// import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+// import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+// import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
+        // UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
+	Underline,
 	BlockQuote,
-	CKFinder,
-	EasyImage,
+	// CKFinder,
+	// EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
+        ImageResize,
 	Indent,
+        IndentBlock,
 	Link,
 	List,
-	MediaEmbed,
+	// MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
 	Table,
 	TableToolbar,
+	Font,
+
+	SimpleUploadAdapter,
+        Alignment,
+        RemoveFormat,
+
 	TextTransformation
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
+        fontSize: {
+            options: [
+                8,
+                10,
+                12,
+                'default',
+                16,
+                18,
+                20,
+                24,
+                28,
+                32,
+                40,
+                50
+            ]
+        },
+
 	toolbar: {
 		items: [
 			'heading',
-			'|',
+                        'fontSize', 'fontFamily', 
 			'bold',
 			'italic',
-			'link',
+                        'underline',
+			'|',
 			'bulletedList',
 			'numberedList',
+                        'alignment',
+			'|',
+                        'fontColor', 'fontBackgroundColor',
+			'insertTable',
 			'|',
 			'indent',
 			'outdent',
-			'|',
-			'imageUpload',
 			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
+			'|',
+			'link',
+			'imageUpload',
+		        // 'mediaEmbed',
+                        'removeformat',
+			'|',
 			'undo',
-			'redo'
+			'redo',
 		]
 	},
+
 	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
+            styles: [
+                'alignLeft', 'alignCenter', 'alignRight'
+            ],
+
+            // Configure the available image resize options.
+            resizeOptions: [
+                {
+                    name: 'imageResize:original',
+                    label: 'Original',
+                    value: null
+                },
+                {
+                    name: 'imageResize:50',
+                    label: '50%',
+                    value: '50'
+                },
+                {
+                    name: 'imageResize:75',
+                    label: '75%',
+                    value: '75'
+                }
+            ],
+
+            toolbar: [
+                'imageStyle:alignLeft', 
+                'imageStyle:alignCenter', 
+                'imageStyle:alignRight',
+                '|',
+                'imageResize',
+                '|',
+                'imageTextAlternative'
+            ]
+
 	},
 	table: {
 		contentToolbar: [
@@ -97,6 +166,37 @@ ClassicEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
+
+        // fontFamily: {
+        //     options: [
+        //        'default',
+        //        'Ubuntu, Arial, sans-serif',
+        //        'Ubuntu Mono, Courier New, Courier, monospace'
+        //    ]
+        // },
+
+        fontFamily : {
+            supportAllValues: true,
+            options: [
+		'default',
+		'Arial, Helvetica, sans-serif',
+                'Arial Black, Helvetica, sans-serif',
+		'Courier New, Courier, monospace',
+                'Comic Sans MS, Helvetica, sans-serif',
+		'Georgia, serif',
+                'Impact, Arial Black, Arial, sans-serif',
+		'Lucida Sans Unicode, Lucida Grande, sans-serif',
+		'Tahoma, Geneva, sans-serif',
+		'Times New Roman, Times, serif',
+		'Trebuchet MS, Helvetica, sans-serif',
+                'Ubuntu, Arial, sans-serif',
+                'Ubuntu Mono, Courier New, Courier, monospace',
+		'Verdana, Geneva, sans-serif'
+	    ]
+        },
+
+
+
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
